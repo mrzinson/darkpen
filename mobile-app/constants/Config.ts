c49ef976482a5f703f979e61f33060d2212dfa30
iconman743-netizen <iconman743@gmail.com>
2026-05-17 21:08:37 +0300

    chore: exclude mobile-app and .env, prepare admin and backend for hosting

--- mobile-app/constants/Config.ts
+++
@@ -1,9 +0,0 @@
-import { Platform } from 'react-native';
-
-const Config = {
-  // Adeegso 'localhost' haddii aad web ku jirto si looga fogaado xannibaadaha browser-ka (CORS/HTTPS)
-  // Haddiise aad mobile dhab ah isticmaalayso, isticmaal IP-gaaga (10.129.6.225)
-  API_URL: Platform.OS === 'web' ? 'http://localhost:5000' : 'http://10.129.6.225:5000',
-};
-
-export default Config;
